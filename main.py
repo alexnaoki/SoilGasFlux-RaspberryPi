78fc9486365fc002f2b0b63d97a4e59775a05a05
Alex Kobayashi <alexkobayashi10@gmail.com>
2024-01-08 17:16:23 +0100

    #5 initial progress

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import machine, os, time, json
-from meteo_pico import Meteo_Pico
+# from meteo_pico import Meteo_Pico
+from init_sensors import Init_Sensors
 from init_motor import Init_Motor
 from init_sd_rtc import Init_SD_RTC
+from aux.create_config import create_config_file
+from prototypes import logging_error
 
+
+### Configurations files ###
 os.chdir('/')
+try:
+    with open('config.json', 'r') as f:
+        config_file = f.read()
+        config = json.loads(config_file)
+except:
+    print('config.json not found')
+    create_config_file()
+    with open('config.json', 'r') as f:
+        config_file = f.read()
+        config = json.loads(config_file)
+###########################
 
-with open('config.json', 'r') as f:
-    config_file = f.read()
-    config = json.loads(config_file)
+### Assigning Buttons ###
+b1 = machine.Pin(config['buttons']['button01'], machine.Pin.IN, machine.Pin.PULL_DOWN)
+b2 = machine.Pin(config['buttons']['button02'], machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 led = machine.Pin(25, machine.Pin.OUT)
+###########################
 
-# RESET_CAUSE = {
-#     'PWRON_RESET': 1,
-#     'HARD_RESET': 2,
-#     'WDT_RESET': 3,
-#     'DEEPSLEEP_RESET': 4,
-#     'SOFT_RESET': 5,
-# }
-# import machine
-# led = machine.Pin("LED", machine.Pin.OUT)
-# led.off()
-# led.on()
-# relay = machine.Pin(15, machine.Pin.OUT)
+### Initialize Components ###
+sd_rtc_component = Init_SD_RTC()
+sd = sd_rtc_component.set_SDCard()      #/sd mounted
+rtc = sd_rtc_component.set_rtc()        #clock object
 
-# led = machine.Pin("LED", machine.Pin.OUT)
-# button01 = machine.Pin(6, machine.Pin.IN, machine.Pin.PULL_DOWN)
-
+print(rtc.datetime())
+print('ok')
 motor = Init_Motor()
 #############################
 print('SENSORS:')
