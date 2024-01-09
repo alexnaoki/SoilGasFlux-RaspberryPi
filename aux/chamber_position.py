64c5151b8676d8649fe61b0567959900ad287426
Alex Kobayashi <alexkobayashi10@gmail.com>
2024-01-09 14:27:15 +0100

    #13 function to check the limit switches

---
+++ aux/chamber_position.py
@@ -0,0 +1,31 @@
+import machine
+
+'''
+
+'''
+class ChamberPosition:
+    def __init__(self, config):
+        limit_swiches_CONFIG = config['limit_swiches']
+        relays_CONFIG = config['relays']
+        
+        self.limit_switch_BOTTOM = machine.Pin(limit_swiches_CONFIG['bottom'], machine.Pin.IN)
+        self.limit_switch_TOP = machine.Pin(limit_swiches_CONFIG['top'], machine.Pin.IN)
+        
+
+    def check_limit_switches(self):
+        if self.limit_switch_BOTTOM.value() == 1:
+            return 'bottom'
+        elif self.limit_switch_TOP.value() == 1:
+            return 'top'
+        else:
+            return None
+
+    # def from_restart(self):
+    #     limit_switch_STATE = self.check_limit_switches()
+        
+    #     if limit_switch_STATE != None:
+    #         return limit_switch_STATE
+    #     else:
+    #         pass
+        
+        
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from init_sensors import Init_Sensors
 from init_motor import Init_Motor
 from init_sd_rtc import Init_SD_RTC
 from aux.create_config import create_config_file
+from aux.chamber_position import Chamber_Position
 from prototypes import logging_error
 
 
@@ -37,13 +38,25 @@ print(rtc.datetime())
 print('ok')
 motor = Init_Motor()
 #############################
+
+### Initialize Sensors ###
 print('SENSORS:')
 sensors = Init_Sensors()
 pressure_sensor = sensors.set_PressureSensor()
 temp_hum_sensor = sensors.set_TempAndHumidity()
 co2_sensor = sensors.set_k30()
+##########################
 
 
+### Initialize Chamber ###
+initial_chamber_position = Chamber_Position(config=config)
+chamber_position = initial_chamber_position.check_chamber_position()
+if chamber_position == 'bottom':
+    print('Chamber is in the bottom')
+elif chamber_position == 'top':
+    print('Chamber is in the top')
+else:
+    print('Chamber not pressing limit switch')
 
 
 
@@ -51,7 +64,7 @@ while True:
     time.sleep(0.5)
     print('nada')
 
-    motor.Rotate_ButtonControl(b1.value(), b2.value())
+    # motor.Rotate_ButtonControl(b1.value(), b2.value())
 
 
 
