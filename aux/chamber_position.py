37c5b1217d9e1b127eef942463392fc79e79bd5c
Alex Kobayashi <alexkobayashi10@gmail.com>
2024-01-10 13:55:46 +0100

    bug fix for the limit switch due to double initialization

--- aux/chamber_position.py
+++ aux/chamber_position.py
@@ -4,12 +4,14 @@ import machine
 
 '''
 class ChamberPosition:
-    def __init__(self, config):
-        limit_swiches_CONFIG = config['limit_swiches']
-        relays_CONFIG = config['relays']
+    def __init__(self, limit_switch_bot, limit_switch_top):
+        # limit_swiches_CONFIG = config['limit_swiches']
+        # relays_CONFIG = config['relays']
         
-        self.limit_switch_BOTTOM = machine.Pin(limit_swiches_CONFIG['bottom'], machine.Pin.IN)
-        self.limit_switch_TOP = machine.Pin(limit_swiches_CONFIG['top'], machine.Pin.IN)
+        # self.limit_switch_BOTTOM = machine.Pin(limit_swiches_CONFIG['bottom'], machine.Pin.IN)
+        # self.limit_switch_TOP = machine.Pin(limit_swiches_CONFIG['top'], machine.Pin.IN)
+        self.limit_switch_BOTTOM = limit_switch_bot
+        self.limit_switch_TOP = limit_switch_top
         
 
     def check_limit_switches(self):
