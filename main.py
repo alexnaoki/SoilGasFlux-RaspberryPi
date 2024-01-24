93771ddbb6e14fd0ab9c38b89a5db98e2a68443d
Alex Kobayashi <alexkobayashi10@gmail.com>
2024-01-24 16:39:30 +0100

    #18 create data folder inside /sd and save metadata and raw data inside this data file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import machine, os, time, json
-# from meteo_pico import Meteo_Pico
 from init_sensors import Init_Sensors
 from init_motor import Init_Motor
 from init_sd_rtc import Init_SD_RTC
 from aux.create_config import create_config_file
 from aux.chamber_position import ChamberPosition
 from prototypes import logging_error
+import components.rtc as rtc
 
 
 ### Configurations files ###
@@ -71,9 +71,9 @@ print(os.listdir('/'))
 ### Initialize Components ###
 sd_rtc_component = Init_SD_RTC()
 sd = sd_rtc_component.set_SDCard()      #/sd mounted
-rtc = sd_rtc_component.set_rtc()        #clock object
+clock_rtc = sd_rtc_component.set_rtc()        #clock object
 
-print(rtc.datetime())
+print(clock_rtc.datetime())
 print('ok')
 motor = Init_Motor()
 #############################
@@ -116,6 +116,17 @@ print('\n\n######## Motor stopped ########\n\n')
 time.sleep(3)
 
 last_action = None
+
+# os.mkdir('/sd/data', exist_ok=True)
+print(os.listdir('/sd'))
+if 'data' in os.listdir('/sd/'):
+    print('existe')
+else:
+    os.mkdir('/sd/data')
+    print('/sd/data created')
+    
+    
+print(f'Entering in the loop at {clock_rtc.datetime().hour}')
 while True:
     if top_ls_pressed and (last_action != 'waiting'):
         motor_current_state = 'stop'
@@ -150,17 +161,69 @@ while True:
         print('Bot limit switch pressed')
         print('#########################')
         print('Starting measuring')
+        
+        start_measure = clock_rtc.datetime()
+        start_measure_time = f'{start_measure.year}-{start_measure.month:02}-{start_measure.day:02}_{start_measure.hour:02}-{start_measure.minute:02}-{start_measure.second:02}'
+        filename = f'{start_measure_time}.json'
+        bmp_pressure = []
+        bmp_temperature = []
+        si_temperature = []
+        si_humidity = []
+        k30_co2 = []
+        datetime = []
+        datetime_utc = []
         while measuring_time < config['timeLimits']['maxTime_chamber_CLOSE']:
             print(measuring_time)
             # time.sleep(0.25)
-            print('bmp280\t',pressure_sensor.pressure, pressure_sensor.temperature)
-            time.sleep(0.25)
-            print('si7021\t', temp_hum_sensor.temperature(), temp_hum_sensor.humidity())
-            time.sleep(0.25)
-            print('k30\t', co2_sensor.read_value())
-            time.sleep(0.5)
+            clock_now_tuple = clock_rtc.datetime()
+            clock_rtc_value = rtc.tuple2seconds(clock_now_tuple)
             
+            bmp_pressure_value = pressure_sensor.pressure
+            bmp_temperature_value = pressure_sensor.temperature
+            
+            si_temperature_value = temp_hum_sensor.temperature()
+            si_humidity_value = temp_hum_sensor.humidity()
+            
+            co2_value = co2_sensor.read_value()
+            
+            bmp_pressure.append(bmp_pressure_value)
+            bmp_temperature.append(bmp_temperature_value)
+            si_humidity.append(si_humidity_value)
+            si_temperature.append(si_temperature_value)
+            k30_co2.append(co2_value)
+            
+            datetime.append(clock_rtc_value)
+            datetime_utc.append(f'{clock_now_tuple.year}-{clock_now_tuple.month:02}-{clock_now_tuple.day:02} {clock_now_tuple.hour:02}:{clock_now_tuple.minute:02}:{clock_now_tuple.second:02}')
+            
+            print(clock_now_tuple)
+            print(rtc.tuple2seconds(clock_now_tuple))
+            print('bmp280\t',bmp_pressure_value, bmp_temperature_value)
+            # time.sleep(0.25)
+            print('si7021\t', si_temperature_value, si_humidity_value)
+            # time.sleep(0.25)
+            print('k30\t', co2_value)
+            # time.sleep(0.5)
+            time.sleep(1)
             measuring_time += 1
+            # bmp_pressure.append(pressure_sensor.pressure)
+        end_measure = clock_rtc.datetime()
+        end_measure_time = f'{end_measure.year}-{end_measure.month:02}-{end_measure.day:02}_{end_measure.hour:02}-{end_measure.minute:02}-{end_measure.second:02}'
+        
+        # print(datetime)
+        # print(datetime_utc)
+        # print(k30_co2)
+        
+        raw_data = {'bmp_pressure': bmp_pressure, 'bmp_temperature': bmp_temperature,
+                    'si_temperature': si_temperature, 'si_humidity': si_humidity,
+                    'k30_co2': k30_co2, 'datetime': datetime, 'datetime_utc': datetime_utc}
+        
+        metadata = {'id_sensor': config['id_sensor'], 'start_time': start_measure_time, 'end_time': end_measure_time}
+        
+        to_json = {'metadata': metadata, 'raw_data': raw_data}
+        
+        with open(f'/sd/data/{filename}', 'w') as f:
+            json.dump(to_json, f)
+        print(f'{filename} created')
         
         print('#########################')
         print('FINISHED MEASURING')
